0ea2d056c4aca37739af47460e74be51e14be6db
kdnakt <a.kid.1985@gmail.com>
2019-09-26 08:13:10 +0900

    fix sql

--- db/migrations/20190917083501-create_user.sql
+++ db/migrations/20190917083501-create_user.sql
@@ -1,12 +1,12 @@
 -- Sample migration
 -- +migrate Up
-CREATE TABLE ‘user‘ (
-‘id‘ bigint unsigned PRIMARY KEY AUTO_INCREMENT,
-‘name‘ varchar(255) NOT NULL,
-‘email‘ varchar(255) NOT NULL,
-‘created_at‘ datetime,
-‘updated_at‘ datetime)
+CREATE TABLE user (
+id bigint unsigned PRIMARY KEY AUTO_INCREMENT,
+name varchar(255) NOT NULL,
+email varchar(255) NOT NULL,
+created_at datetime,
+updated_at datetime)
 ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC
 ;
 -- +migrate Down
-DROP TABLE IF EXISTS ‘user‘;
+DROP TABLE IF EXISTS user;
